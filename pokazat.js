b27445bc12a45219068dd42c0b1383056c700d3e
Den <den9357600@gmail.com>
2022-10-26 00:43:41 +0300

    Get rid of text to number and number to text transformation

--- pokazat.js
+++ pokazat.js
@@ -1,59 +1,26 @@
-function transformNumericDirectionsToText(numericDirections) {
-  return numericDirections.map((direction) => {
-    if (direction === 2) {
-      return ('NORTH');
-    }
-
-    if (direction === -2) {
-      return ('SOUTH');
-    }
-
-    if (direction === 1) {
-      return ('WEST');
-    }
-
-    if (direction === -1) {
-      return ('EAST');
-    }
-
-    return '';
-  });
-}
+const directionMap = {
+  NORTH: 2,
+  SOUTH: -2,
+  WEST: 1,
+  EAST: -1,
+};
 
 function dirReduc(arr) {
-  const gg = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === 'NORTH') {
-      gg.push(2);
-    } else if (arr[i] === 'SOUTH') {
-      gg.push(-2);
-    } else if (arr[i] === 'WEST') {
-      gg.push(1);
-    } else if (arr[i] === 'EAST') {
-      gg.push(-1);
+  for (let i = 0; i < arr.length; i += 1) {
+    if (directionMap[arr[i]] + directionMap[arr[i + 1]] === 0) {
+      /**
+       * Make array copy, because .splice mutate "arr"
+       * "arr" is the argument of dirReduc
+       * mutating arguments is not good
+       */
+      const reducedArray = [...arr];
+      reducedArray.splice(i, 2);
+
+      return dirReduc(reducedArray);
     }
   }
 
-  console.log(gg);
-
-  // КАК ЗАМЕНИТЬ ЭТО? -----------------------------------------------------------------------------
-  for (let i = 0; i < gg.length; i++) {
-    if (gg[i] + gg[i + 1] === 0) {
-      gg.splice(i, 2);
-
-      const textDirections = transformNumericDirectionsToText(gg);
-
-      return dirReduc(textDirections);
-    }
-  }
-  // КАК ЗАМЕНИТЬ ЭТО? -----------------------------------------------------------------------------
-
-  console.log(gg);
-
-  const final = transformNumericDirectionsToText(gg);
-  console.log(final);
-
-  return final;
+  return arr;
 }
 
 // Test
